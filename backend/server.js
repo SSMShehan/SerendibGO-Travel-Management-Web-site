1977dde677b7712deb042a4c79f81455df08ed3d
SSMshehan <shehanmadusanka775@gmail.com>
2025-12-30 02:15:29 +0530

    Allow deployed frontend URL in CORS

--- backend/server.js
+++ backend/server.js
@@ -111,7 +111,8 @@ app.use(cors({
       'http://localhost:3002',
       'http://127.0.0.1:3000',
       'http://127.0.0.1:3001',
-      'http://127.0.0.1:3002'
+      'http://127.0.0.1:3002',
+      'https://serendib-go-travel-management-web-s-weld.vercel.app'
     ];
 
     // Add production frontend URL from environment variable
